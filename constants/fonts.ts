ef12caee7094d06830cc7b5de31eb9c5005522ba
judymoody59 <jmj.chai@gmail.com>
2025-08-26 02:30:45 +0900

    KU-27/chore: Paperlogy 기반 전역 폰트 설정
    - Paperlogy 폰트 전역 등록
    - constants/fonts.ts에 전역 폰트 정의

---
+++ constants/fonts.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const fonts = StyleSheet.create({
+  largeTitle: { fontFamily: 'Paperlogy-Medium', fontSize: 25 },
+  mediumTitle: { fontFamily: 'Paperlogy-Bold', fontSize: 23 },
+  smallTitle: { fontFamily: 'Paperlogy-Bold', fontSize: 21 },
+  normalText: { fontFamily: 'Paperlogy-Medium', fontSize: 19 },
+  largeText: { fontFamily: 'Paperlogy-Medium', fontSize: 20 },
+  mediumText: { fontFamily: 'Paperlogy-Medium', fontSize: 17 },
+  smallText: { fontFamily: 'Paperlogy-Medium', fontSize: 15 },
+});
