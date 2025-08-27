6992132374d32752bde384e1f63ab5dd555e0bde
MinJu Chai <108432112+judymoody59@users.noreply.github.com>
2025-08-27 16:43:26 +0900

    Ku 37/chore: 프로젝트 전역 색상 관리 (#8)
    * KU-37/chore: 전역 색상 상수 정의
    
    * KU-37/fix: 로그인 화면에 전역 색상 적용

---
+++ constants/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+
+  orange: "#D84925", // 주요 서비스 컬러(주황색)
+  lightOrange: "#FFD2C7",
+  gray: "#B7B7B7",
+  lightGray: "#F6F6F6",
+  green: "#2B8268",
+  red: "#EF4444",
+  white: "#FFFFFF",
+  black: "#000000",
+
+} as const;
