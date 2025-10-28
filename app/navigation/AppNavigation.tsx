85f897b158993fc1d20fbb718bdfdbdaecdc12b4
judymoody59 <jmj.chai@gmail.com>
2025-10-28 14:05:36 +0900

    KU-59/feat: 전역 NavigationRef 및 AppNavigator 탭 전환 로직 구현

---
+++ app/navigation/AppNavigation.tsx
@@ -0,0 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { navigationRef } from "./NavigationRef";
+
+import CreatePartyScreen from "../create-party";
+import PartyListScreen from "../party-list";
+import MainScreen from "../main-s";
+import EventListScreen from "../event-list";
+import MyScreen from "../my-s";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="CreatePartyScreen" component={CreatePartyScreen} />
+                <Stack.Screen name="PartyListScreen" component={PartyListScreen} />
+                <Stack.Screen name="MainScreen" component={MainScreen} />
+                <Stack.Screen name="EventListScreen" component={EventListScreen} />
+                <Stack.Screen name="MyScreen" component={MyScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+
+}
