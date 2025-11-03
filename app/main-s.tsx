ac76c41bed872667754f254203540e1dda2a4bb6
judymoody59 <jmj.chai@gmail.com>
2025-11-03 12:29:25 +0900

    KU-79/feat: EventSearchBar UI 구현

--- app/main-s.tsx
+++ app/main-s.tsx
@@ -1,11 +1,13 @@
 import { View } from "react-native";
 import AppHeader from "@/components/bars/AppHeader";
 import AppNavigator from "./navigation/AppNavigator";
+import EventSearchBar from "@/components/bars/EventSearchBar";
 
 export default function MainScreen(){
     return (
         <View className="flex-1 bg-white">
             <AppHeader />
+            <EventSearchBar />
         </View>
     );
 }
---
+++ components/bars/EventSearchBar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, TextInput, TouchableOpacity, Keyboard } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { fonts } from "@/constants/fonts";
+import { colors } from "@/constants/colors";
+
+export default function EventSearchBar() {
+    const [query, setQuery] = React.useState("");
+
+    //추후 API 연동 시 수정
+    const handleSearch = () => {
+        console.log("검색!", query);
+        Keyboard.dismiss();
+    };
+
+    return (
+        <View
+            className="flex-row items-center rounded-xl px-4 h-[60px] ml-5 mr-5 mt-10 bg-white border"
+            style={{ borderColor: colors.gray }}
+        >
+            <TextInput
+                className="flex-1 text-base"
+                style={[
+                    fonts.mediumText,
+                    {color: colors.black}
+                ]}
+                placeholder="가고 싶은 행사를 입력해주세요."
+                placeholderTextColor={colors.gray}
+                returnKeyType="search"
+                blurOnSubmit={false}
+                onSubmitEditing={handleSearch}
+            />
+            <TouchableOpacity onPress={handleSearch}>
+                <Feather name="search" size={22} color={colors.darkGray} />
+            </TouchableOpacity>
+        </View>
+
+    )
+
+}
