acc756ffcb07ef23248efa57bf9399c340c515d4
judymoody59 <jmj.chai@gmail.com>
2025-11-12 10:36:40 +0900

    KU-80/fix: types.ts 라우트 인식 오류 해결

--- app/navigation/AppNavigator.tsx
+++ app/navigation/AppNavigator.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import { colors } from "@/constants/colors";
-import type { RootTabParamList } from "./types";
+import type { RootTabParamList } from "./_types";
 
 import CreatePartyScreen from "../create-party";
 import PartyListScreen from "../party-list";
