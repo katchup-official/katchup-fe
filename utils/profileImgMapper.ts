e3bd25ccee56d5c3a6a1b48fb9cd8ff72650a869
judymoody59 <jmj.chai@gmail.com>
2025-11-30 18:44:34 +0900

    KU-88/refactor: dateTime formatter, profileImgMapper를 utils로 분리

---
+++ utils/profileImgMapper.ts
@@ -0,0 +1,6 @@
+export const profileImages: Record<number, any> = {
+  1: require("@/assets/images/profiles/tomato.png"),
+  2: require("@/assets/images/profiles/flower.png"),
+  3: require("@/assets/images/profiles/ketchup.png"),
+  4: require("@/assets/images/profiles/basket.png"),
+};
